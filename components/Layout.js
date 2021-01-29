0d2df48411cfc595d9174b7c69e8dc5ad3982d9f
black.dragon74 <nickk.2974@gmail.com>
2021-01-29 15:48:24 +0530

    Fix ScrollToTop exceptions
    Signed-off-by: black.dragon74 <nickk.2974@gmail.com>

--- components/Layout.js
+++ components/Layout.js
@@ -2,8 +2,25 @@ import Header from './Header'
 import NavBar from './NavBar'
 import PropTypes from 'prop-types'
 import ScrollToTop from './ScrollToTop'
+import {useEffect} from "react";
 
 const Layout = ({title, children}) => {
+
+    // This is the right place for this as we wanna add listener
+    // Only when the layout is rendered (it won't be rendered on 404)
+    // In such cases, having it in `_app.js` will throw exception
+    useEffect(() => {
+        if (typeof window === "undefined")
+            return
+
+        document.getElementById('scrollToTop').hidden = true
+        window.addEventListener('scroll', scrollListener)
+
+        return () => {
+            removeEventListener('scroll', scrollListener)
+        }
+    })
+
     return (
         <>
             <Header title={title}/>
@@ -16,6 +33,13 @@ const Layout = ({title, children}) => {
             <ScrollToTop/>
         </>
     )
+}
+
+function scrollListener(event) {
+    let sTop = document.documentElement.scrollTop
+    const el = document.getElementById('scrollToTop')
+
+    el.hidden = sTop <= 100;
 }
 
 Layout.propTypes = {
--- pages/_app.js
+++ pages/_app.js
@@ -9,29 +9,15 @@ import 'aos/dist/aos.css'
 // Lock and load
 function MyApp({Component, pageProps}) {
     useEffect(() => {
-        document.getElementById('scrollToTop').hidden = true
         // Sad but AOS doesn't return an object on init, so can't destroy
         AOS.init({
             duration: 1000,
             easing: "ease-in-out-back",
             once: true
         });
-
-        window.addEventListener('scroll', scrollListener)
-
-        return () => {
-            removeEventListener('scroll', scrollListener)
-        }
     })
 
     return <Component {...pageProps} />
-}
-
-function scrollListener(event) {
-    let sTop = document.documentElement.scrollTop
-    const el = document.getElementById('scrollToTop')
-
-    el.hidden = sTop <= 100;
 }
 
 export default MyApp
